a99d81c045fc385f13b3baca31f9899b0e54e7c9
CoraLi1 <lirong1221@hotmail.com>
2016-09-07 19:41:06 +0800

    Update config.php

--- jdkinfo/config.php
+++ jdkinfo/config.php
@@ -2,7 +2,7 @@
 	 /**
       * 微信公众号配置信息
       */
-     define('APP_ID','wx23984ec7ec5350d3');            					//AppID(应用ID)
-     define('APP_SECRET','1e238a371f2f1a7f48c434b6a9378e96');           //AppSecret(应用密钥)
-     //define('APP_URL','http://www.yinruifang.cn/6index.html');           //调用微信接口的页面url
+     define('APP_ID','wxd7672be1a66da1d2');            					//AppID(应用ID)
+     define('APP_SECRET','04df821b046576c2ff0c36cd61e07282');           //AppSecret(应用密钥)
+     define('APP_URL','http://corali1.github.io/web');           //调用微信接口的页面url
 ?>
