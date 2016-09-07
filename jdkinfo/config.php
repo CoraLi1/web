32470f70e616779354b3243776ec05ffd71b0f1d
CoraLi1 <lirong1221@hotmail.com>
2016-09-07 19:51:45 +0800

    Update config.php

--- jdkinfo/config.php
+++ jdkinfo/config.php
@@ -4,5 +4,5 @@
       */
      define('APP_ID','wxd7672be1a66da1d2');            					//AppID(应用ID)
      define('APP_SECRET','04df821b046576c2ff0c36cd61e07282');           //AppSecret(应用密钥)
-     define('APP_URL','http://corali1.github.io/web');           //调用微信接口的页面url
+     define('APP_URL','http://corali1.github.io');           //调用微信接口的页面url
 ?>
